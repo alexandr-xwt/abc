c04242a080a09f550605e3636e8dca2d74770320
Александр Попов <alexandr-bkt@yandex.ru>
2021-04-27 17:39:33 +0500

    add ...

--- task-1.php
+++ task-1.php
@@ -4,3 +4,6 @@ $a = 'Группа инженеров из Национального центр
 $link = 'https://radiovesti.ru/brand/60950/episode/2530336/';
 // 180 symb
 $b = mb_substr($a,0,180,'UTF-8');
+// ...
+$b = $b . '...';
+echo $b;
