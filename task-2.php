efe744daf81fcb9e80bbf2b26179c18736d92f52
Александр Попов <alexandr-bkt@yandex.ru>
2021-04-29 03:08:18 +0500

    Создание изображения 20000*20000

--- task-2.php
+++ task-2.php
@@ -1,2 +1,12 @@
 <?php
 
+// Создание изображения 20000*20000 
+$im = imagecreate(20000, 20000);
+
+// Серый фон
+$bg = imagecolorallocate($im, 160, 160, 160);
+
+// Вывод изображения
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
