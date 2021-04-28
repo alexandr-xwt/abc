a425585cafe677c495c94f0bdf7b30f69190d117
Александр Попов <alexandr-bkt@yandex.ru>
2021-04-29 03:50:08 +0500

    Сохранение изображения

--- task-2.php
+++ task-2.php
@@ -1,12 +1,12 @@
 <?php
 
-// Создание изображения 20000*20000 
-$im = imagecreate(20000, 20000);
-
-// Серый фон
+// Создание изображения 20000*20000
+// $im = imagecreate(20000, 20000);
+// Создание изображения 800*800
+$im = imagecreate(800, 800);
+// Первый вызов: серый фон
 $bg = imagecolorallocate($im, 160, 160, 160);
-
-// Вывод изображения
-header('Content-type: image/png');
-imagepng($im);
+// Сохранение изображения
+// header('Content-type: image/png');
+imagepng($im, __DIR__ . '/image.png');
 imagedestroy($im);
